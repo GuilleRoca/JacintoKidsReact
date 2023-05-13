8b5e53cc3fcd34ca0eadbe01c2cdcef8d5b1c3ba
Guillermo Rocatti <grocatti@hotmail.com>
2023-05-13 03:12:15 -0300

    creacion de app con react

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+			<footer className="bg-warning-subtle text-center text-lg-start d-flex" >
+				<div>INFO</div>
+				<div>MENU</div>
+				<div>REDES</div>
+			</footer>
+        
+    </>
+  )
+}
+
+export default Footer
