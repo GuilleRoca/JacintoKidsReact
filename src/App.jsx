09818cfb2baff4e77ce5acdf8c7ef32a29028ed8
Guillermo Rocatti <grocatti@hotmail.com>
2023-05-17 00:56:34 -0300

    creacion de componentes basicos

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Footer from './components/Footer'
 import HeaderLogin from './components/HeaderLogin'
 import NavBar from './components/NavBar'
+import { Navigate, Route, Routes } from 'react-router-dom'
 
 function App() {
 
@@ -10,6 +11,18 @@ function App() {
       <div className="App">
         <HeaderLogin/>  
         <NavBar/>
+        <Routes >
+          <Route path="/" element={<Navigate to="/home" />}/>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/productos" element={<ItemListContainer/>}/>
+          <Route path="/productos/item/:id" element={<ItemDetail/>}/>
+          <Route path="/productos/category/:cat" element={<ItemListContainer/>}/>
+          <Route path="/contacto" element={<Contacto/>}/>
+          <Route path="/ubicacion" element={<Ubicacion/>}/>
+          <Route path="/carrito" element={<Cart/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/registrar" element={<CreateUser/>}/>
+        </Routes>
         <Footer/>
       </div>
     </>
--- src/components/HeaderLogin/index.jsx
+++ src/components/HeaderLogin/index.jsx
@@ -6,29 +6,6 @@ import style from './headerLogin.module.scss'
 
 const HeaderLogin = () => {
   
-  const [page1, setPage1] = useState("carousel-item active")
-  const [page2, setPage2] = useState("carousel-item")
-  const [page3, setPage3] = useState("carousel-item")
-  
-  useEffect(() => {
-    
-    setTimeout(() => {
-      setPage1("carousel-item")
-      setPage2("carousel-item active")
-      setPage3("carousel-item")
-    }, 5000)
-    setTimeout(() => {
-      setPage1("carousel-item")
-      setPage2("carousel-item")
-      setPage3("carousel-item active")
-    }, 10000)
-    setTimeout(() => {
-      setPage1("carousel-item active")
-      setPage2("carousel-item")
-      setPage3("carousel-item")
-    }, 15000)
-
-    }, [])
   
 
   return (
@@ -46,15 +23,15 @@ const HeaderLogin = () => {
           <div className={style.slider}>
           <div className={style.slideTrack}>
           <div className={style.slide}>
-            <p className="m-0 p-0 align-middle">Nueva temporada Spring-Summer !! <img src="./sol.png" alt="sol" className={style.img}/><img src="./pelota-de-playa.png" alt="pelota" className={style.img}/></p>
+            <p className="m-0 p-0 align-middle">Hasta 40% de descuento en la temporada Spring-Summer !!  <img src="./sol.png" alt="sol" className={style.img}/>  <img src="./pelota-de-playa.png" alt="pelota" className={style.img}/></p>
           </div>
 
           <div className={style.slide}>
-            <p className="m-0 p-0 align-middle">3 y 6 cuotas sin interes con todas las tarjetas de crédito!!<img src="./promocion.png" alt="promo" className={style.img}/></p>
+            <p className="m-0 p-0 align-middle">3 y 6 cuotas sin interes con todas las tarjetas de crédito !!  <img src="./promocion.png" alt="promo" className={style.img}/></p>
           </div>
 
           <div className={style.slide}>
-            <p className="m-0 p-0 align-middle">Hasta 40% de descuento en la temporada otoño-invierno!!<img src="./nieve.png" alt="nieve" className={style.img}/><img src="./muneca.png" alt="muñeco de nieve" className={style.img}/></p>
+            <p className="m-0 p-0 align-middle">Nueva temporada otoño-invierno !!  <img src="./nieve.png" alt="nieve" className={style.img}/>  <img src="./muneca.png" alt="muñeco de nieve" className={style.img}/></p>
           </div>
           </div>
           </div>
